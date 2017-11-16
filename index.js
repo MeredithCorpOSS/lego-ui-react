ee6f267190ad0ddfc95e4ee635b7efccc30e3090
awebdeveloper <iprathik@gmail.com>
2017-11-16 13:05:31 +0530

    Components exported

---
+++ index.js
@@ -0,0 +1,11 @@
+import Button from "./components/Button/Button"
+import Input from "./components/Input/Input"
+import OverUnderLine from "./components/OverUnderLine/OverUnderLine"
+import Pagination from "./components/Pagination/Pagination"
+
+export {
+  Button,
+  Input,
+  OverUnderLine,
+  Pagination
+}
