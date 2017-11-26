12c2586605a89dbd88a5b36cb913ef8148b013d4
awebdeveloper <iprathik@gmail.com>
2017-11-26 23:38:11 +0530

    Webpack config

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import Button from "./components/Button/Button"
 import Input from "./components/Input/Input"
+import Select from "./components/Select/Select"
 import OverUnderLine from "./components/OverUnderLine/OverUnderLine"
 import Pagination from "./components/Pagination/Pagination"
+import Dropdown from "./components/Dropdown/Dropdown"
+import SvgIcons from "./components/SvgIcons/SvgIcons"
 
 export {
   Button,
   Input,
+  Select,
   OverUnderLine,
-  Pagination
+  Pagination,
+  Dropdown,
+  SvgIcons
 }
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+var path = require('path');
+const autoprefixer = require('autoprefixer');
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'components'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // @remove-on-eject-begin
+            babelrc: false,
+            presets: [require.resolve('babel-preset-react-app')],
+            // @remove-on-eject-end
+            compact: true,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebookincubator/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9', // React doesn't support IE8 anyway
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      },
+    ]
+  },
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+    },
+    {
+      'PropTypes': {
+        root: 'PropTypes',
+        commonjs2: 'prop-types',
+        commonjs: 'prop-types',
+        amd: 'prop-types',
+      },
+    },
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
